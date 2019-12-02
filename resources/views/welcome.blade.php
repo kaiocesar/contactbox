401fee2eced00c01f6a31828973cb781d6a4c85d
Kaio Cesar <tecnico.kaio@gmail.com>
2019-12-02 15:42:34 -0300

    view contactbox

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ContactBox
                 </div>
 
                 <div class="links">
