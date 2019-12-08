a8e80f40e27be5bc799ee9950122077f412af2da
Kaio Cesar <tecnico.kaio@gmail.com>
2019-12-08 11:19:59 -0300

    DB unit tests

--- tests/Unit/ContactTest.php
+++ tests/Unit/ContactTest.php
@@ -12,24 +12,41 @@ use Tests\TestCase;
 class ContactTest extends TestCase {
 
     public function testDBObterContatos() {
-        $contacts = DB::select('SELECT * FROM contacts');
+        $contacts = Contact::where('status', '1')->get();
         $this->assertTrue( count( $contacts ) == 1 );
     }
 
     public function testDBGravarContatoAdicionado() {
-        $this->assertTrue( false );
+        $contact = new Contact();
+        $contact->name = 'Juliana Evangelista';
+        $contact->activity = 'Farmaceutica';
+        $contact->mobile = '1330030001';
+        $contact->email = 'julianaevangelista@email.com';
+        $contact->last_contact = '2019-12-04';
+        $contact->status = '1';
+        $contact->save();
+
+        $this->assertArrayHasKey('created_at', $contact);
     }
 
     public function testDBObterContatoParaEditar() {
-        $this->assertTrue( false );
+        $contacts = Contact::where('id', '1')->get();
+        $this->assertTrue( count( $contacts ) > 0);
     }
 
     public function testDBGravarContatoEditado() {
-        $this->assertTrue( false );
+        $contacts = Contact::where('id', '1')->get();
+        if ( count( $contacts ) ) {
+            $contacts[0]->name = 'Juliana Evangelista';
+            $contacts[0]->save();
+        }
+        $this->assertTrue( $contacts[0]->name == 'Juliana Evangelista' );
     }
 
     public function testDBDeletarContato() {
-        $this->assertTrue( false );
+        Contact::where('id', '1')->delete();
+        $contacts = Contact::where('id', '1')->get();
+        $this->assertTrue( count( $contacts ) == 0 );
     }
 
 }
