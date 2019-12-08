8ef618eb87588a779c12c4a7aaaa2962057b9db7
Kaio Cesar <tecnico.kaio@gmail.com>
2019-12-08 11:28:05 -0300

    red test failed

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -9,27 +9,42 @@ class ContactTest extends TestCase {
 
     public function testAPIObterContatos() {
         $response = $this->get( '/api/contacts' );
-        $response->assertStatus( 200 );
+        $response->assertStatus( 200 )
+            ->assertJson([
+                'created_at' => '2019-12-08'
+            ]);
     }
 
     public function testAPIObterContato() {
         $response = $this->get( '/api/contacts/1' );
-        $response->assertStatus( 200 );
+        $response->assertStatus( 200 )
+            ->assertJson([
+                'created_at' => '2019-12-08'
+            ]);
     }
 
     public function testAPIGravarNovoContato() {
         $response = $this->post( '/api/contacts' );
-        $response->assertStatus( 200 );
+        $response->assertStatus( 200 )
+            ->assertJson([
+                'created_at' => '2019-12-08'
+            ]);
     }
 
     public function testAPIGravarContatoEditado() {
         $response = $this->put( '/api/contacts/1' );
-        $response->assertStatus( 200 );
+        $response->assertStatus( 200 )
+            ->assertJson([
+                'created_at' => '2019-12-08'
+            ]);
     }
 
     public function testAPIDeletarContato() {
         $response = $this->delete( '/api/contacts/1' );
-        $response->assertStatus( 200 );
+        $response->assertStatus( 200 )
+            ->assertJson([
+                'created_at' => '2019-12-08'
+            ]);
     }
 
 }
