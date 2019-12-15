bd22aad33695c29bba1115cf16f626a94d32e618
Kaio Cesar <tecnico.kaio@gmail.com>
2019-12-15 17:54:11 -0300

    add graphQL

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Rebing\GraphQL\GraphQLServiceProvider::class,
+
     ],
 
     /*
@@ -227,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'GraphQL' => 'Rebing\GraphQL\Support\Facades\GraphQL',
 
     ],
 
