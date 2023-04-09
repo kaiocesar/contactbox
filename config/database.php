a6d90811300286c6c72cc7101a9547525733ab70
Kaio Cesar <tecnico.kaio@gmail.com>
2023-04-09 15:48:11 -0300

    updating docker settings

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url( env('DATABASE_URL') );
-
 return [
     'default' => env( 'DB_CONNECTION', 'mysql' ),
 
@@ -11,6 +9,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'url'   => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_HOST', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -31,4 +30,33 @@ return [
     ],
 
     'migrations' => 'migrations',
+
+    'redis' => [
+
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        ],
+
+        'default' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+        ],
+
+        'cache' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+
+    ],
 ];
