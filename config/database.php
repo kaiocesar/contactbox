62e564f9de77cb1f691dd8448103ee67bee941bb
Kaio Cesar <tecnico.kaio@gmail.com>
2023-04-09 15:32:52 -0300

    Changing database

--- config/database.php
+++ config/database.php
@@ -5,18 +5,18 @@ use Illuminate\Support\Str;
 $DATABASE_URL = parse_url( env('DATABASE_URL') );
 
 return [
-    'default' => env( 'LARA_DB_CONNECTION', 'mysql' ),
+    'default' => env( 'DB_CONNECTION', 'mysql' ),
 
     'connections' => [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('LARA_DB_HOST', '127.0.0.1'),
-            'port' => env('LARA_DB_PORT', '3306'),
-            'database' => env('LARA_DB_DATABASE', 'forge'),
-            'username' => env('LARA_DB_USERNAME', 'forge'),
-            'password' => env('LARA_DB_PASSWORD', ''),
-            'unix_socket' => env('LARA_DB_SOCKET', ''),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_HOST', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -26,19 +26,6 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'require',
         ],
 
     ],
